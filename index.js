da423eed7dc216438ecd83d49d6ca2b2ed96a8f2
Witold Jas <jaswitold1@gmail.com>
2022-02-01 10:27:30 +0100

    feature/loader implemented

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
 const frenchUsers = () => {
   let loader = `<div class="boxLoading">LOADING</div>`;
   document.querySelector(".placeholder").innerHTML = loader;
-  console.log(loader);
+
   fetch("https://randomuser.me/api/?results=10&gender=male&nat=fr")
     .then((resp) => resp.json())
-    .then((resp) => console.log(resp));
+    .then(
+      (resp) =>
+        (document.querySelector(".placeholder").innerHTML = `
+          <div>
+          ${resp.info.seed}
+          </div>
+          `)
+    );
 };
 
 frenchUsers();
